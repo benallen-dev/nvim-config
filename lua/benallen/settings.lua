f000d3b7095230a0506ed8ee3ab53e19defafae3
Ben Allen <ben@benallen.dev>
2023-08-28 19:19:37 +0200

    set listchars, correct indenting in packer.lua

--- lua/benallen/settings.lua
+++ lua/benallen/settings.lua
@@ -1,7 +1,14 @@
--- vim.opt.guicursor = ""
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.smartindent = true
 vim.opt.signcolumn = "yes"
+
+vim.opt.list = true
+
+vim.opt.listchars = {
+	-- eol = "󰌑",
+	tab = " ",
+	trail = "",
+	lead = "",
+}
