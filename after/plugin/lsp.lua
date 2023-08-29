4c869d88043e7c6ec3e6f996bdd389554193d219
Ben Allen <ballen@anwb.nl>
2023-08-29 16:28:58 +0200

    Add <leader>lf shortcut to format file with LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,21 +3,16 @@ local lsp = require('lsp-zero').preset({})
 lsp.on_attach(function(client, bufnr)
 	-- see :help lsp-zero-keybindings
 	-- to learn the available actions
-	lsp.default_keymaps({buffer = bufnr})
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
---[[
-lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-})
-
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 
+	-- Primeagen's keymaps
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -28,7 +23,12 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+	-- These are mine
+	vim.keymap.set("n", "<leader>lf", function()
+		vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+	end, opts)
 end)
 
---]]
+
 lsp.setup()
